287950367bc3aaa992792ed36c10def976a8adb3
tha-ree-jan <thareejap.20@uom.lk>
2023-10-09 20:45:49 +0530

    initiated and completed routes

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div style={{ backgroundColor: '#1E1E1E', color: '#72FC41', textAlign: 'center', fontSize: '100px', fontFamily: 'Iceland' }}>
+            404 Error
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/pages/Play.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Play = () => {
+    return ( 
+        <div>
+           hjkegherkg 
+        </div>
+     );
+}
+ 
+export default Play;
